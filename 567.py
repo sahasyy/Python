f9a3a89844b4b75793bfb1aa73f841ffa663e83f
Sahas Sharma <124092524+sahasyy@users.noreply.github.com>
2024-07-28 17:48:23 -0500

    Add files via upload

---
+++ 567.py
@@ -0,0 +1,9 @@
+
+
+ 
+import socket   
+hostname = socket.gethostname()   
+IPAddr = socket.gethostbyname(hostname)   
+print("Your Computer Name is:" + hostname)   
+print("Your Computer IP Address is:" + IPAddr)
+      
